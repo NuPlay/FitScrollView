5ce6d278c997b8b376fb341606ed114c86edb602
LeeUngjae <73557895+NuPlay@users.noreply.github.com>
2021-08-23 19:06:45 +0900

    init

---
+++ FitScrollView/README.md
@@ -0,0 +1,3 @@
+# FitScrollView
+
+A description of this package.
---
+++ FitScrollView/Tests/FitScrollViewTests/FitScrollViewTests.swift
@@ -0,0 +1,11 @@
+//    import XCTest
+//    @testable import FitScrollView
+//
+//    final class FitScrollViewTests: XCTestCase {
+//        func testExample() {
+//            // This is an example of a functional test case.
+//            // Use XCTAssert and related functions to verify your tests produce the correct
+//            // results.
+//            XCTAssertEqual(FitScrollView().text, "Hello, World!")
+//        }
+//    }
